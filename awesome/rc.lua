ee11341a4ce2142b0b9613e5044c0a055ed22c2e
rxy0424 <rxy0424@gmail.com>
2024-04-28 23:18:03 +0800

    AwesomeWM: use xorg-xrdb and dex for XGD Autostart
    xorg-xrdb to detect the first boot, dex to autostart

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -570,4 +570,9 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
-awful.spawn.with_shell("dex --environment Awesome --autostart")
+awful.spawn.with_shell(
+    'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
+    'xrdb -merge <<< "awesome.started:true";' ..
+    -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
+    'dex --environment Awesome --autostart'
+    )
